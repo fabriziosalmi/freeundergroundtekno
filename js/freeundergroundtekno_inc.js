924b805f057688550cf8f68deac170af0964a959
fab <fabrizio.salmi@gmail.com>
2020-02-26 14:11:35 +0100

    Create freeundergroundtekno_inc.js

---
+++ js/freeundergroundtekno_inc.js
@@ -0,0 +1,20 @@
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-5693861-2"></script>
+<script>
+  var OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "ac6ac1a6-d1bb-49b9-ab10-dffcbe6d9810",
+      notifyButton: {
+        enable: true,
+      },
+    });
+  });
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-5693861-2');
+</script>
